5e16c5726b4649c3ee287dd478b83f8c83862686
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-27 09:34:37 -0600

    Add API call for placing new order

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3002/api/v1/orders')
       .then(response => response.json())
 }
+
+export const placeOrder = (newOrder) => {
+  return fetch('http://localhost:3002/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/jsoon"
+    },
+    body: JSON.stringify(newOrder)
+  })
+    .then(response => response.json())
+}
